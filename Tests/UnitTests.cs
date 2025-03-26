64e0d52113263019dc5c6f177d8fb470acd43c6c
dianaxhanz <dhanzlick@gmail.com>
2025-03-26 15:25:58 -0500

    Update UnitTests.cs #2
    Added Multiply and Divide Unit Tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -57,3 +57,59 @@ public class Subtract
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
 	}
 }
+
+ // Multiplication Unit Test
+public class Multiply
+{
+	[TestMethod]
+	public void Add_Valid_Patino()
+	{
+		Assert.AreEqual(3, Program.Multiply("1", "2"));
+		Assert.AreEqual(5, Program.Multiply("3", "2"));
+		Assert.AreEqual(12, Program.Multiply("5", "7"));
+	}
+
+	[TestMethod]
+	public void Add_Invalid_Patino()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "a"));
+	}
+
+	[TestMethod]
+	public void Add_Null_Patino()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, null));
+	}
+}
+
+ // Division Unit Test
+public class Divide
+{
+	[TestMethod]
+	public void Add_Valid_Patino()
+	{
+		Assert.AreEqual(3, Program.Divide("1", "2"));
+		Assert.AreEqual(5, Program.Divide("3", "2"));
+		Assert.AreEqual(12, Program.Divide("5", "7"));
+	}
+
+	[TestMethod]
+	public void Add_Invalid_Patino()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Divide("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Divide("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Divide("a", "a"));
+	}
+
+	[TestMethod]
+	public void Add_Null_Patino()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, null));
+	}
+}
